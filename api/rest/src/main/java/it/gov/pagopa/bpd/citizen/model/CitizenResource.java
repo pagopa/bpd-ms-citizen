8f85c55dca09e34e0f63b1e25e3252c4ea06880f
alecastagno <alessandro.castagno.sia@gmail.com>
2020-09-14 16:21:47 +0200

    Added swagger descriptions

--- api/rest/src/main/java/it/gov/pagopa/bpd/citizen/model/CitizenResource.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/citizen/model/CitizenResource.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.bpd.citizen.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -10,10 +12,20 @@ import java.time.OffsetDateTime;
 @EqualsAndHashCode(of = "fiscalCode", callSuper = false)
 public class CitizenResource {
 
+    @ApiModelProperty(value = "${swagger.citizen.fiscalCode}", required = true)
+    @JsonProperty(required = true)
     private String fiscalCode;
+    @ApiModelProperty(value = "${swagger.citizen.payoffInstr}", required = true)
+    @JsonProperty(required = true)
     private String payoffInstr;
+    @ApiModelProperty(value = "${swagger.citizen.payoffInstrType}", required = true)
+    @JsonProperty(required = true)
     private String payoffInstrType;
+    @ApiModelProperty(value = "${swagger.citizen.timestampTC}", required = true)
+    @JsonProperty(required = true)
     private OffsetDateTime timestampTC;
+    @ApiModelProperty(value = "${swagger.citizen.enabled}", required = true)
+    @JsonProperty(required = true)
     private boolean enabled;
 
 }
