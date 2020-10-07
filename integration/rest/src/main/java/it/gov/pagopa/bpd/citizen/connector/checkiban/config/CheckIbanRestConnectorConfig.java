7f39c8853f1d90c4bc7783987215daf4264f3a9c
alecastagno <alessandro.castagno.sia@gmail.com>
2020-10-07 12:31:05 +0200

    Fix Proxy Configuration

--- integration/rest/src/main/java/it/gov/pagopa/bpd/citizen/connector/checkiban/config/CheckIbanRestConnectorConfig.java
+++ integration/rest/src/main/java/it/gov/pagopa/bpd/citizen/connector/checkiban/config/CheckIbanRestConnectorConfig.java
@@ -1,19 +1,12 @@
 package it.gov.pagopa.bpd.citizen.connector.checkiban.config;
 
-import feign.Client;
 import it.gov.pagopa.bpd.citizen.connector.checkiban.CheckIbanRestClient;
 import it.gov.pagopa.bpd.common.connector.config.RestConnectorConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-
-import javax.net.ssl.SSLSocketFactory;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 
 @Configuration
 @Import(RestConnectorConfig.class)
@@ -21,33 +14,5 @@ import java.net.Proxy;
 @PropertySource("classpath:config/BpdCitizenRestConnector.properties")
 @Slf4j
 public class CheckIbanRestConnectorConfig {
-//
-//    @Value("${rest-client.checkiban.proxy.enabled}")
-//    private Boolean proxyEnabled;
-//
-//    @Value("${rest-client.checkiban.proxy.host}")
-//    private String proxyHost;
-//
-//    @Value("${rest-client.checkiban.proxy.port}")
-//    private Integer proxyPort;
-//
-//    @Bean
-//    public Client getFeignClient() throws Exception {
-//        try {
-//            SSLSocketFactory sslSocketFactory = null;
-//
-//            if (proxyEnabled) {
-//                Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
-//                    return new Client.Proxied(sslSocketFactory,null, proxy);
-//            } else {
-//                return new Client.Default(sslSocketFactory, null);
-//            }
-//
-//        } catch (Exception e) {
-//            log.error(e.getMessage(),e);
-//            throw new Exception("Error occured while initializing feign client", e);
-//        }
-//
-//
-//    }
+
 }
--- integration/rest/src/main/java/it/gov/pagopa/bpd/citizen/connector/checkiban/config/ProxyOkHttpClientFactory.java
+++ integration/rest/src/main/java/it/gov/pagopa/bpd/citizen/connector/checkiban/config/ProxyOkHttpClientFactory.java
@@ -1,12 +1,21 @@
 package it.gov.pagopa.bpd.citizen.connector.checkiban.config;
 
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.commons.httpclient.DefaultOkHttpClientFactory;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 
 public class ProxyOkHttpClientFactory extends DefaultOkHttpClientFactory {
+
+    @Value("${rest-client.checkiban.proxy.host}")
+    private String proxyHost;
+
+    @Value("${rest-client.checkiban.proxy.port}")
+    private Integer proxyPort;
+
+
     public ProxyOkHttpClientFactory(OkHttpClient.Builder builder) {
         super(builder);
     }
@@ -14,7 +23,7 @@ public class ProxyOkHttpClientFactory extends DefaultOkHttpClientFactory {
     @Override
     public OkHttpClient.Builder createBuilder(boolean disableSslValidation) {
         OkHttpClient.Builder builder = super.createBuilder(true);
-        builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("10.70.66.101", 8181)));
+        builder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort)));
         return builder;
     }
 }
