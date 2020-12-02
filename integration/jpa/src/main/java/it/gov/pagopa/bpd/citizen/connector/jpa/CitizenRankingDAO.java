66cede5337e9866878454451ed4b7dba045215d2
alecastagno <alessandro.castagno.sia@gmail.com>
2020-12-02 18:25:08 +0100

    fix unsubscribe citizen, deactivation citizen ranking

--- integration/jpa/src/main/java/it/gov/pagopa/bpd/citizen/connector/jpa/CitizenRankingDAO.java
+++ integration/jpa/src/main/java/it/gov/pagopa/bpd/citizen/connector/jpa/CitizenRankingDAO.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.bpd.citizen.connector.jpa;
 import it.gov.pagopa.bpd.citizen.connector.jpa.model.CitizenRanking;
 import it.gov.pagopa.bpd.citizen.connector.jpa.model.CitizenRankingId;
 import it.gov.pagopa.bpd.common.connector.jpa.CrudJpaDAO;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -107,4 +108,16 @@ public interface CitizenRankingDAO extends CrudJpaDAO<CitizenRanking, CitizenRan
                                                    @Param("awardPeriod") Long awardPeriod);
 
 
+    @Modifying
+    @Query("update CitizenRanking " +
+            "set updateDate = CURRENT_TIMESTAMP," +
+            "updateUser = :fiscalCode, " +
+            "totalCashback = 0, " +
+            "transactionNumber = 0, " +
+            "ranking = null, " +
+            "rankingMinRequired = null, " +
+            "maxTotalCashback = null, " +
+            "enabled = false " +
+            "where fiscalCode = :fiscalCode ")
+    void deactivateCitizenRankingByFiscalCode(@Param("fiscalCode") String fiscalCode);
 }
