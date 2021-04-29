07736999507e9e70cdc6a727f90b3e840b5b41f5
mdesogus <michael.desogus.sia@gmail.com>
2021-04-29 16:51:57 +0200

    added test on integration event module

--- integration/event/src/main/java/it/gov/pagopa/bpd/citizen/publisher/model/PaymentInstrumentUpdate.java
+++ integration/event/src/main/java/it/gov/pagopa/bpd/citizen/publisher/model/PaymentInstrumentUpdate.java
@@ -2,11 +2,15 @@ package it.gov.pagopa.bpd.citizen.publisher.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class PaymentInstrumentUpdate {
 
