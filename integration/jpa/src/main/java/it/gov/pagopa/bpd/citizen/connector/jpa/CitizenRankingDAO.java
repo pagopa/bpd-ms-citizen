0d8b17299a139d560df6b169e46b2469b43696e8
mdesogus <michael.desogus.sia@gmail.com>
2021-05-06 15:11:54 +0200

    Updated deploymet-prod configuration

--- integration/jpa/src/main/java/it/gov/pagopa/bpd/citizen/connector/jpa/CitizenRankingDAO.java
+++ integration/jpa/src/main/java/it/gov/pagopa/bpd/citizen/connector/jpa/CitizenRankingDAO.java
@@ -24,7 +24,10 @@ public interface CitizenRankingDAO extends CrudJpaDAO<CitizenRanking, CitizenRan
             "ranking = null, " +
             "rankingMinRequired = null, " +
             "maxTotalCashback = null, " +
-            "enabled = false " +
+            "enabled = false, " +
+            "idTrxPivot = null, " +
+            "cashbackNormPivot = 0, " +
+            "idTrxMinTransactionNumber = null " +
             "where fiscalCode = :fiscalCode ")
     void deactivateCitizenRankingByFiscalCode(@Param("fiscalCode") String fiscalCode);
 }
